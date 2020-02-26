f851af0369d55d588f3bb90028bb6c961556e3e0
jaeyoung.choi <lactrious@gmail.com>
2020-02-27 00:29:51 +0900

    v1.0.5

--- lib/append.d.ts
+++ lib/append.d.ts
@@ -1,3 +1,4 @@
 import Env from "./env";
 import LockOptions from "./support/lockOptions";
-export default function append(env: Env): (key: string, body: string, { noLock }?: LockOptions) => Promise<string>;
+import SyncOptions from "./support/syncOptions";
+export default function append(env: Env): (key: string, body: string, { noLock, sync }?: LockOptions & SyncOptions) => Promise<string>;
--- lib/index.d.ts
+++ lib/index.d.ts
@@ -1,9 +1,10 @@
 import Env from "./env";
 export default function S3cb(env: Env): {
     get: (key: string, { noLock }?: import("./support/lockOptions").default) => Promise<string>;
-    put: (key: string, body: string, { noLock }?: import("./support/lockOptions").default) => Promise<string>;
+    put: (key: string, body: string, { noLock, sync }?: import("./support/lockOptions").default & import("./support/syncOptions").default) => Promise<string>;
     del: (key: string, { noLock }?: import("./support/lockOptions").default) => Promise<string>;
-    append: (key: string, body: string, { noLock }?: import("./support/lockOptions").default) => Promise<string>;
+    append: (key: string, body: string, { noLock, sync }?: import("./support/lockOptions").default & import("./support/syncOptions").default) => Promise<string>;
+    sync: (key: string) => Promise<string>;
     invalidate: (key: string) => Promise<string>;
     lock: (key: string) => Promise<string>;
     unlock: (key: string) => Promise<string>;
